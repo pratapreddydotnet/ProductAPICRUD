c41a7d9f92bc084c8cbdf106993163c36c56f929
pratapreddydotnet <pratapreddydotnet@gmail.com>
2025-09-11 08:51:52 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPICRUD.Models;
+
+namespace ProductAPICRUD.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductAPICRUD.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public string Desc { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
